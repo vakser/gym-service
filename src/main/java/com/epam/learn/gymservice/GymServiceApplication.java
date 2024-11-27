642aca5cd33222024b31d09f491ca33c0a8038b6
vakser <vakals@gmail.com>
2024-11-27 20:22:57 +0200

    Initial commit

---
+++ src/main/java/com/epam/learn/gymservice/GymServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learn.gymservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymServiceApplication.class, args);
+    }
+
+}
